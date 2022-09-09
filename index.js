3205279dc0e9ec0ee36d83b09e2fde8b7e7be346
Abrahamv19 <hugoabrahamv19@gmail.com>
2022-09-09 15:47:38 -0500

    Agrego forEach

--- index.js
+++ index.js
@@ -1,67 +1,41 @@
 class Finca {
   constructor(info) {
-    this.nombre = info.nombreVendedor;
-    this.imagen = info.imagenFinca;
-    this.titulo = info.tituloFinca;
-    this.area = info.areaFinca;
-    this.ubicacion = info.ubicacionFinca;
+    this.id = info.idFinca;
+    this.nombreV = info.nombreVendedor;
+    this.nombreF = info.nombreFinca;
     this.precio = info.precioFinca;
-    this.negociable = info.negociaFinca;
   }
 
   hablar() {
-    let mensaje = `Hola ${arregloFinca1[0]}, los datos que ingresaste de tu finca son los siguentes: Imagen: ${imagenFinca}, Titulo: ${tituloFinca}, Area: ${areaFinca}, Ubicacion: ${ubicacionFinca}, Precio: ${precioFinca}, Negociable: ${negociaFinca}, gracias por usar el portal Fincalaventa.com, hemos registrado tus datos con exito.`;
+    let mensaje = `Hola los datos de las fincas registradas son los siguientes: ID finca 1: ${idFinca}, Nombre del vendedor: ${nombreVendedor}, Nombre de la finca: ${nombreFinca}, Precio: ${precioFinca}, ID finca 2: ${idFinca2}, Nombre del vendedor: ${nombreVendedor2}, Nombre de la finca: ${nombreFinca2}, Precio: ${precioFinca2}, gracias por usar el portal Fincalaventa.com`;
     alert(mensaje);
   }
 }
 
-let nombreVendedor = prompt("Ingresa tu nombre");
-let imagenFinca = prompt("Sube la imagen de tu finca en el siguiente link");
-let tituloFinca = prompt(
-  "Ingresa el titulo que quieres ver en tu publicacion (Max. 30 caracteres)"
-);
-let areaFinca = prompt("Ingresa el area de tu finca y su unidad de medida");
-let ubicacionFinca = prompt(
-  "Ingresa el nombre del municipio donde se encuentra tu finca"
-);
+let idFinca = prompt("Ingresa el ID de la finca");
+let nombreVendedor = prompt("Ingresa el nombre del vendedor");
+let nombreFinca = prompt("Ingresa el nombre de la finca");
 let precioFinca = parseInt(prompt("Ingresa el precio de tu finca"));
-let negociaFinca = prompt(
-  "Si el precio de tu finca es negociable ingresa la palabra SI de lo contrario ingresa la palabra NO"
-).toUpperCase();
 
-const arregloFinca1 = [
-  nombreVendedor,
-  imagenFinca,
-  tituloFinca,
-  areaFinca,
-  ubicacionFinca,
-  precioFinca,
-  negociaFinca,
-];
+const finca1 = new Finca(idFinca, nombreVendedor, nombreFinca, precioFinca);
+const arregloFinca1 = [idFinca, nombreVendedor, nombreFinca, precioFinca];
 
-const finca1 = new Finca(
-  arregloFinca1[0],
-  arregloFinca1[1],
-  arregloFinca1[2],
-  arregloFinca1[3],
-  arregloFinca1[4],
-  arregloFinca1[5],
-  arregloFinca1[6]
+const fincas = [];
+fincas.push(arregloFinca1);
+
+let idFinca2 = prompt("Ingresa el ID de la segunda finca");
+let nombreVendedor2 = prompt("Ingresa el nombre del vendedor");
+let nombreFinca2 = prompt("Ingresa el nombre de la finca");
+let precioFinca2 = parseInt(prompt("Ingresa el precio de tu finca"));
+
+const finca2 = new Finca(idFinca2, nombreVendedor2, nombreFinca2, precioFinca2);
+const arregloFinca2 = [idFinca2, nombreVendedor2, nombreFinca2, precioFinca2];
+
+fincas.push(arregloFinca2);
+
+alert(
+  "A continuación te mostramos un resumen de los datos ingressados hasta el momento"
 );
+fincas.forEach((item) => item.forEach((dato) => alert(dato)));
+
 finca1.hablar();
-
-let cambioNombreVendedor = prompt("Ingresa el nombre del nuevo propietario");
-arregloFinca1.shift();
-arregloFinca1.unshift(cambioNombreVendedor);
-
-const finca2 = new Finca(
-  arregloFinca1[0],
-  arregloFinca1[1],
-  arregloFinca1[2],
-  arregloFinca1[3],
-  arregloFinca1[4],
-  arregloFinca1[5],
-  arregloFinca1[6]
-);
-
-finca2.hablar();
